3087de3fb6ac222963ac41052f4b943188c7e6b7
Mani-877 <vtu28918@veltech.edu.in>
2026-09-03 09:25:26 +0530

    Add rotateString method in Solution class
    Implement rotateString method to check if one string can be rotated to match another.

---
+++ Week 6 task 4 vtu28918.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        return (s + s).contains(goal);
+    }
+}
+
+0utput
+
+s = "m"
+goal = "f"
+
+false
