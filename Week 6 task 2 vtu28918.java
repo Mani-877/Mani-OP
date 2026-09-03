feb328f0a8c107b098c2fee5f3e4a4cb6e7e9b03
Mani-877 <vtu28918@veltech.edu.in>
2026-09-03 09:23:23 +0530

    Add repeatedSubstringPattern method to Solution class
    Implement a method to check for repeated substrings in a string.

---
+++ Week 6 task 2 vtu28918.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+        
+        return doubled.substring(1, doubled.length() - 1).contains(s);
+    }
+}
+
+output
+
+s = "abab"
+true
